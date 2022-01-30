e9d2ad7b8d555c49ea360003a816b82d92d19fd0
Joao Artur <joaoartur4590@gmail.com>
2022-01-30 20:09:56 -0300

    realise2

--- controller.js
+++ controller.js
@@ -1,3 +1,18 @@
+class btns {
+  constructor(text, callback) {
+    const $body = document.querySelector("body");
+    const $button = document.createElement("button");
+    $button.textContent = text;
+
+    callback($button);
+
+    $body.insertAdjacentElement("beforeend", $button);
+
+    return $button;
+  }
+}
+
+let msg = document.querySelector(".msg");
 document.addEventListener("DOMContentLoaded", function () {
   let squares = document.querySelectorAll(".square");
 
@@ -10,11 +25,47 @@ function handleClick(event) {
   let square = event.target;
   let position = square.id;
 
-  handleMove(position);
+  if (handleMove(position)) {
+    setTimeout(function () {
+      msg.innerText = `O vencedor foi o jogador ${playerTime + 1}!`;
+      const createBtns = new btns("Restart", function (button) {
+        button.style.cssText = `
+        margin-left:45vw;
+        border: #111 solid 2px;
+        padding: 20px;
+        font-size:20px;`;
+        button.addEventListener("mouseenter", style);
+        button.addEventListener("mouseleave", style1);
+        button.addEventListener("click", restart);
+        function restart() {
+          document.location.reload();
+        }
+        function style() {
+          button.style.cssText = `
+          margin-left:45vw;
+          font-size:20px;
+              background-color: #111;
+            color: #eee;
+            border: 2px #eee solid;
+            cursor: pointer;
+            padding: 20px;`;
+        }
+        function style1() {
+          button.style.cssText = `
+            margin-left:45vw;
+            border: #111 solid 2px;
+            padding: 20px;
+            font-size:20px;`;
+        }
+      });
+    }, 200);
+  }
   updateSquares();
 }
+msg.innerText = `Vez do jogador ${playerTime + 1}!`;
 
 function updateSquares() {
+  msg.innerText = `Vez do jogador ${playerTime + 1}!`;
   let squares = document.querySelectorAll(".square");
 
   for (let square of squares) {
